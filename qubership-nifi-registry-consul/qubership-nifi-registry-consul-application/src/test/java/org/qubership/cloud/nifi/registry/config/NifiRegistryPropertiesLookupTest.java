9f0d41b6dea7ed2c3ac5e5b7a095cfb95b2bec60
Dmitriy Myasnikov <myasnikov.qubership@gmail.com>
2026-03-16 22:13:09 +0300

    test: fix linter issues in test classes and change quarkus-jacoco report location

--- qubership-nifi-registry-consul/qubership-nifi-registry-consul-application/src/test/java/org/qubership/cloud/nifi/registry/config/NifiRegistryPropertiesLookupTest.java
+++ qubership-nifi-registry-consul/qubership-nifi-registry-consul-application/src/test/java/org/qubership/cloud/nifi/registry/config/NifiRegistryPropertiesLookupTest.java
@@ -1,10 +1,8 @@
 package org.qubership.cloud.nifi.registry.config;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
